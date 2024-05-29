7380cea8c817f92ffbaf7e8378acdba4c284b866
Kevin Alexis Roodriguez Condori <krodriguezcondori34@gmai.com>
2024-05-28 23:54:32 -0400

    feat test validatio button

---
+++ src/ui/components/atoms/publish_button/PublishButton.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import PublishButton from './PublishButton.tsx';
+
+describe('Testing Button React Component', () => {
+  test('should be rendere PUBLICAR ARTICULO', () => {
+    render(<PublishButton >PUBLICAR ARTICULO</PublishButton>);
+    const buttonElement = screen.getByText(/PUBLICAR ARTICULO/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('should have correct styles', () => {
+    render(<PublishButton>PUBLICAR ARTICULO</PublishButton>);
+    const buttonElement = screen.getByRole('button');
+
+    expect(buttonElement).toHaveClass('buttonPublish__button');
+    expect(buttonElement).toHaveStyle({background: '#0793bf'});
+    const labelElement = screen.getByText(/PUBLICAR ARTICULO/i);
+    expect(labelElement).toHaveClass('buttonPublish__label');
+
+    const styles = window.getComputedStyle(labelElement);
+
+    expect(styles.color).toBe('rgb(255, 255, 255)');
+    expect(styles.textTransform).toBe('uppercase');
+
+  });
+});
---
+++ src/ui/components/atoms/publish_button/PublishButton.tsx
@@ -0,0 +1,19 @@
+import './styles.scss';
+import type { IProps } from './types/IProps';
+
+function PublishButton(props: IProps) {
+  const { children } = props;
+  return (
+    <div className="buttonPublish">
+      <button className="buttonPublish__button" style={{
+          background: '#0793bf',
+      }}>
+        <span className="buttonPublish__label" style={{
+            color: '#fff',
+            textTransform: 'uppercase',
+        }}>{children}</span>
+      </button>
+    </div>
+  );
+}
+export default PublishButton;
